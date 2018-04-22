7270a84705885eacb5bea3f494e807840c39097c
Alex <alexxsur@hotmail.com>
2018-04-22 14:08:43 -0500

    seccion para crear productos

---
+++ src/app/components/producto-add.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { ProductoService } from '../services/producto.service';
+import { Producto } from '../models/producto';
+
+@Component({
+    selector: 'producto-add',
+    templateUrl: '../views/producto-add.html',
+    providers: [ProductoService]
+})
+export class ProductoAddComponent{
+    public titulo: string;
+
+    constructor(){
+        this.titulo = 'Crear un nuevo producto';
+    }
+
+    ngOnInit(){
+        console.log('producto-add.component.ts cargado...');
+    }
+}
+
