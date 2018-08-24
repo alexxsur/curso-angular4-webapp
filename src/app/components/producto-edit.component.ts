9241d8ea1ed231baa8ba80597bb6656849df8acb
Alex <alexxsur@gmail.com>
2018-08-23 20:43:49 -0500

    Formulario para actualizar productos

--- src/app/components/producto-edit.component.ts
+++ src/app/components/producto-edit.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ProductoService } from '../services/producto.service';
 import { Producto } from '../models/producto';
+import { GLOBAL } from '../services/global';
 
 @Component({
     selector: 'producto-edit',
@@ -13,6 +14,7 @@ export class ProductoEditComponent{
     public producto: Producto;
     public filesToUpload;
     public resultoUpload;
+    public is_edit;
 
     constructor(
         private _productoService: ProductoService,
@@ -21,5 +23,72 @@ export class ProductoEditComponent{
     ){
         this.titulo = 'Editar un producto';
         this.producto = new Producto(0,'','',0,'');
+        this.is_edit = true;
+    }
+
+    ngOnInit(){
+        console.log('producto-edit.component.ts  cargado...');
+        this.getProducto();
+    }
+
+    getProducto(){
+        this._route.params.forEach((params: Params) => {
+            let id = params['id'];
+
+            this._productoService.getProducto(id).subscribe(
+                response => {
+                    if(response.code == 200){
+                        this.producto = response.data;
+                    }else{
+                        this._router.navigate(['/productos']);
+                    }
+                },
+                error => {
+                    console.log(<any>error);
+                }
+            );
+        });
+    }
+    onSubmit(){
+        console.log(this.producto);
+
+        if( this.filesToUpload && this.filesToUpload.length >= 1){
+            this._productoService.makeFileRequest(GLOBAL.url+'upload-file', [], this.filesToUpload).then((result) => {
+                console.log(result);
+                this.resultoUpload = result;
+                this.producto.imagen = this.resultoUpload.filename;
+                this.updateProducto();
+             }, (error)=>{
+                console.log(error);
+             });
+        }else{
+            this.updateProducto();
+        }
+
+
+
+    }
+
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log(this.filesToUpload);
+    }
+
+    updateProducto(){
+        this._route.params.forEach((params: Params) => {
+            let id = params['id'];
+            this._productoService.editProducto(id, this.producto).subscribe(
+                response => {
+                    if(response.code == 200){
+                        this._router.navigate(['/producto',id]);
+                    }else{
+                        console.log(response);
+                    }
+                },
+                error => {
+                    console.log(<any>error);
+                }
+            );
+        });
     }
 }
